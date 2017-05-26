91d1a88d8b93cbd3d8ba5d82084b93a5cce6b80d
Florian Hammerschmidt <fh@cca.io>
2017-05-26 11:01:01 +0200

    Upgrading dependencies.

--- android/app/src/main/java/com/beaconbulletin/MainApplication.java
+++ android/app/src/main/java/com/beaconbulletin/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.mackentoch.beaconsandroid.BeaconsAndroidPackage;
 import com.mmazzarolo.beaconsandroid.BeaconsAndroidPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BeaconsAndroidPackage(),
           new BeaconsAndroidPackage(),
           new VectorIconsPackage()
       );
