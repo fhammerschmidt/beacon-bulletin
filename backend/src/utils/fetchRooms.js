aa80f2cc9d419a92fd9afd109aa183b8beaf3a65
Florian Hammerschmidt <fh@cca.io>
2019-01-02 23:35:50 +0100

    Always fetch rooms from BachApi iff there are no rooms in db

--- backend/src/utils/fetchRooms.js
+++ backend/src/utils/fetchRooms.js
@@ -5,35 +5,35 @@ import { Room } from '../api/models';
 import storeBachApiRoom from './storeBachApiRoom';
 
 export default function fetchRooms() {
-  axios
-    .get('https://bach.wu.ac.at/d/rr/api/rooms/', {
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
+  // Only populate database with rooms from BachApi if there are none.
+  Room.estimatedDocumentCount()
+    .then(count => {
+      if (count < 1) {
+        return axios
+          .get('https://bach.wu.ac.at/d/rr/api/rooms/', {
+            headers: {
+              'Content-Type': 'application/json; charset=utf-8',
+            },
+          })
+          .then(res => res.data.filter(room => room.category === 'Project room').map(room => storeBachApiRoom(room)))
+          .then(rooms => Room.insertMany(rooms, { ordered: false }, (err, _rooms) => console.warn(err)));
+      } else {
+        return null;
+      }
     })
-    .then(res => {
-      res.data
-        .filter(room => room.category === 'Project room')
-        .map(room => {
-          const newRoom = new Room(storeBachApiRoom(room));
-          return newRoom.save((err, created) =>
-            err ? console.log(err) : console.log(created, ' stored successfully')
-          );
-        });
 
-      // eslint-disable-next-line array-callback-return
-      // rooms.map(room => {
-      //   axios
-      //     .get(`https://campus.wu.ac.at/de/search/${room}?format=json`)
-      //     .then(res1 => {
-      //       // Room properties are stored in the GET responses features array (it is from a map feature).
-      //       data.push(res1.data.features[0].properties);
-      //     })
-      //     .catch(err => {
-      //       console.log('Error retrieving room properties', err);
-      //     });
-      // });
-    })
+    // eslint-disable-next-line array-callback-return
+    // rooms.map(room => {
+    //   axios
+    //     .get(`https://campus.wu.ac.at/de/search/${room}?format=json`)
+    //     .then(res1 => {
+    //       // Room properties are stored in the GET responses features array (it is from a map feature).
+    //       data.push(res1.data.features[0].properties);
+    //     })
+    //     .catch(err => {
+    //       console.log('Error retrieving room properties', err);
+    //     });
+    // });
     .catch(err => {
       console.log('Error retrieving rooms:', err);
     });
